2d1438ec5afc1d429cd489f0fac7f78484d798b4
Samuel-wei <67288441@qq.com>
2020-11-05 14:55:13 +0800

    first add codes

---
+++ making_pizzas.py
@@ -0,0 +1,7 @@
+# utf-8
+# learning Python
+
+import pizza
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
---
+++ print_models.py
@@ -0,0 +1,10 @@
+# utf-8
+# learning Python
+
+import printing_functions as pf
+
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+pf.print_models(unprinted_designs[:], completed_models)
+pf.show_completed_models(completed_models)
